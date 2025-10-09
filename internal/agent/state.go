48a3a1c17e30f62a054dff5b918033696fcac4a2
ngeorgy <ngeorgy@google.com>
2025-10-09 15:35:09 +0200

    Change agentinternal package to agent (#147)

--- internal/agent/state.go
+++ internal/agent/state.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package agentinternal
+package agent
 
 // holds Agent internal state
 type Agent interface {
